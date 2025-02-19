c46d78fe4f05cacf2569340e8dcf61be74bea4bd
lyka-ed <lykaedem11@gmail.com>
2025-02-19 16:35:25 +0100

    leetcode challenge

---
+++ Max Number of K-Sum Pairs/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function (nums, k) {
+  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+  let operations = 0;
+
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+    if (sum === k) {
+      operations++;
+      left++;
+      right--;
+    } else if (sum < k) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return operations;
+};
---
+++ Max Number of K-Sum Pairs/index.ts
@@ -0,0 +1,22 @@
+function maxOperations(nums: number[], k: number): number {
+    nums.sort((a, b) => a - b);
+    let left = 0;
+    let right = nums.length - 1;
+    let operations = 0;
+
+    while (left < right) {
+        const sum = nums[left] + nums[right];
+
+        if (sum === k) {
+            operations++; 
+            left++;
+            right--;
+        } else if (sum < k) {
+            left++; 
+        } else {
+            right--; 
+        }
+    }
+
+    return operations;
+}
