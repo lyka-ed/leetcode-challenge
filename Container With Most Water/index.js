2e5f543702dfb4e1c482f67f7fbd598e35795cca
lyka-ed <lykaedem11@gmail.com>
2025-02-19 16:18:05 +0100

    update

--- Container With Most Water/index.js
+++ Container With Most Water/index.js
@@ -2,25 +2,25 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    let maxArea = 0;
-    let left = 0;
-    let right = height.length - 1;
+var maxArea = function (height) {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
 
-    while (left < right) {
-        const minHeight = Math.min(height[left], height[right]);
-        const width = right - left;
-        const area = minHeight * width;
-        maxArea = Math.max(maxArea, area);
+  while (left < right) {
+    const minHeight = Math.min(height[left], height[right]);
+    const width = right - left;
+    const area = minHeight * width;
+    maxArea = Math.max(maxArea, area);
 
-        if (height[left] < height[right]) {
-            left++;
-        } else {
-            right--;
-        }
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
     }
-    return maxArea;
+  }
+  return maxArea;
 };
 
-const height = [1, 2, 0, 11, 23, 9, 6];
+const height = [1, 2, 0, 7, 6];
 console.log("Maximum area:", maxArea(height));
