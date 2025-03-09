bca271b734e40a2665a2e668417c4e97999ce43f
lyka-ed <lykaedem11@gmail.com>
2025-03-09 07:08:50 +0100

    decode string

--- Decode String/index.js
+++ Decode String/index.js
@@ -1,32 +1,4 @@
 /**
- * @param {number[]} asteroids
- * @return {number[]}
- */
-function asteroidCollision(asteroids) {
-  const stack = [];
-
-  for (let asteroid of asteroids) {
-    let isDestroyed = false;
-
-    while (stack.length > 0 && asteroid < 0 && stack[stack.length - 1] > 0) {
-      let top = stack.pop();
-      if (top > Math.abs(asteroid)) {
-        stack.push(top);
-        isDestroyed = true;
-        break;
-      } else if (top === Math.abs(asteroid)) {
-        isDestroyed = true;
-        break;
-      }
-    }
-
-    if (!isDestroyed) stack.push(asteroid);
-  }
-
-  return stack;
-}
-
-/**
  * @param {string} s
  * @return {string}
  */
